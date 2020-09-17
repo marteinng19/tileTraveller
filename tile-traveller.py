c9175f7c261b710b02a8cf1a935613232d04c4d7
Marteinn Guðmundsson <marteinng19@ru.is>
2020-09-17 16:26:48 +0000

    commit Marteinn

--- tile-traveller.py
+++ tile-traveller.py
@@ -20,8 +20,34 @@ def victory():
     print("Victory!")
     return running
 
-def print_pos():
-    print(position_x,position_y)
+def compass(direction):
+    if direction == "n":
+        y = north(y)
+        return y
+    if direction == "s":
+        y = south(y)
+        return y
+    if direction == "e":
+        x = east(x)
+        return x
+    if direction == "w":
+        x = west(x)
+        return x
+    
+
+
+
+    
+def print_north():
+    return "(N)orth"
+def print_south():
+    return "(S)outh"
+def print_east():
+    return "(E)ast"
+def print_west():
+    return "(W)est"
+    
+        
 
 def ui():
     direction = str(input("Direction: "))
@@ -30,7 +56,7 @@ def ui():
 
 
 
-#values
+#variables
 running = True
 position_x = 1
 position_y = 1
@@ -40,21 +66,17 @@ while running == True:
     direction = ui()
     if direction != "n" and direction != "s" and direction != "e" and direction != "w":
         print("Not a valid directon!")
-    if direction == "n":
-        position_y = north(position_y)
-        print_pos()
-    if direction == "s":
-        position_y = south(position_y)
-        print_pos()
-    if direction == "e":
-        position_x = east(position_x)
-        print_pos()
-    if direction == "w":
-        position_x = west(position_x)
-        print_pos()
+    position = compass(direction)
+    if position_x == 1 and position_y == 1:
+        print("You can travel {}".format(print_north()))
+    if position_x == 1 and position_y == 2:
+        print("You can travel {} or {} or {}.".format(print_north(),print_south(),print_east()))
+    if position_x == 1 and position_y == 3:
+        print("You can travel {} or {}.".format(print_south(),print_east()))    
     if position_x == 3 and position_y == 1:
         victory()
         running = False
+    
     
     
 
