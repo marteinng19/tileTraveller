b832ef5715a8e8a1fe363a81742b67d5943568a8
Marteinn Guðmundsson <marteinng19@ru.is>
2020-09-17 15:36:19 +0000

    eitthvad

--- tile-traveller.py
+++ tile-traveller.py
@@ -34,31 +34,27 @@ def ui():
 running = True
 position_x = 1
 position_y = 1
-direction = str(input("Direction: "))
+
 
 while running == True:
-    ui()
-    if direction == "n" or direction == "s" or direction == "e" or direction == "w":
-        if direction == "n":
-            position_y = north(position_y)
-            print_pos()
-
-        if direction == "s":
-            position_y = south(position_y)
-            print_pos()
-            
-        if direction == "e":
-            position_x = east(position_x)
-            print_pos()
-            
-        if direction == "w":
-            position_x = west(position_x)
-            print_pos()
-            
-        if position_x == 3 and position_y == 1:
-            victory()
-    else:
-        print("Not a valid direction!")
-        
+    direction = ui()
+    if direction != "n" and direction != "s" and direction != "e" and direction != "w":
+        print("Not a valid directon!")
+    if direction == "n":
+        position_y = north(position_y)
+        print_pos()
+    if direction == "s":
+        position_y = south(position_y)
+        print_pos()
+    if direction == "e":
+        position_x = east(position_x)
+        print_pos()
+    if direction == "w":
+        position_x = west(position_x)
+        print_pos()
+    if position_x == 3 and position_y == 1:
+        victory()
+        running = False
+    
     
 
