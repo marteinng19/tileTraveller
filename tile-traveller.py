5ea2c2d0a3457df59f8553514b1f6b22c0801702
Marteinn Guðmundsson <marteinng19@ru.is>
2020-09-17 13:46:19 +0000

    skeleton

--- tile-traveller.py
+++ tile-traveller.py
@@ -1,1 +1,16 @@
-print("hæ")
+def north(direction):
+
+def south(direction):
+
+def east(direction):
+
+def west(direction):
+
+
+running = True
+
+while running:
+    
+    if tile == 31:
+        running = False
+        print("Victory!")
