ef90b3dc0649c6fe8baafd2670400d20bf5ff7fa
Marteinn Guðmundsson <marteinng19@ru.is>
2020-09-18 11:53:16 +0000

    Final 1

--- tile-traveller.py
+++ tile-traveller.py
@@ -19,24 +19,6 @@ def victory():
     running = False
     print("Victory!")
     return running
-
-def compass(direction):
-    if direction == "n":
-        y = north(y)
-        return y
-    if direction == "s":
-        y = south(y)
-        return y
-    if direction == "e":
-        x = east(x)
-        return x
-    if direction == "w":
-        x = west(x)
-        return x
-    
-
-
-
     
 def print_north():
     return "(N)orth"
@@ -47,8 +29,7 @@ def print_east():
 def print_west():
     return "(W)est"
     
-        
-
+    
 def ui():
     direction = str(input("Direction: "))
     direction = direction.lower()
@@ -63,16 +44,85 @@ position_y = 1
 
 
 while running == True:
-    direction = ui()
-    if direction != "n" and direction != "s" and direction != "e" and direction != "w":
-        print("Not a valid directon!")
-    position = compass(direction)
+
     if position_x == 1 and position_y == 1:
         print("You can travel {}".format(print_north()))
+        direction = ui()
+        if direction != "n":
+            print("Not a valid directon!")
+        elif direction == "n":
+            position_y = north(position_y)
+
     if position_x == 1 and position_y == 2:
         print("You can travel {} or {} or {}.".format(print_north(),print_south(),print_east()))
+        direction = ui()
+        if direction != "n" and direction != "s" and direction != "e":
+            print("Not a valid directon!")
+        elif direction == "n":
+            position_y = north(position_y)
+        elif direction == "s":
+            position_y = south(position_y)
+        elif direction == "e":
+            position_x = east(position_x)
+    
     if position_x == 1 and position_y == 3:
-        print("You can travel {} or {}.".format(print_south(),print_east()))    
+        print("You can travel {} or {}.".format(print_south(),print_east())) 
+        direction = ui()
+        if direction != "s" and direction != "e":
+            print("Not a valid directon!")
+        elif direction == "s":
+            position_y = south(position_y)
+        elif direction == "e":
+            position_x = east(position_x)
+
+    if position_x == 2 and position_y == 1:
+        print("You can travel {}.".format(print_north()))
+        direction = ui()
+        if direction != "n":
+            print("Not a valid directon!")
+        elif direction == "n":
+            position_y = north(position_y)
+
+    if position_x == 2 and position_y == 2:
+        print("You can travel {} or {}.".format(print_west(),print_south()))
+        direction = ui()
+        if direction != "w" and direction != "s":
+            print("Not a valid directon!")
+        elif direction == "w":
+            position_x = west(position_x)
+        elif direction == "s":
+            position_y = south(position_y)
+
+    if position_x == 2 and position_y == 3:
+        print("You can travel {} or {}.".format(print_east(),print_west()))
+        direction = ui()
+        if direction != "e" and direction != "w":
+            print("Not a valid directon!")
+        elif direction == "w":
+            position_x = west(position_x)
+        elif direction == "e":
+            position_x = east(position_x)
+
+    if position_x == 3 and position_y == 3:
+        print("You can travel {} or {}.".format(print_south(),print_east()))
+        direction = ui()
+        if direction != "s" and direction != "e":
+            print("Not a valid directon!")
+        elif direction == "s":
+            position_y = south(position_y)
+        elif direction == "e":
+            position_x = east(position_x)
+
+    if position_x == 3 and position_y == 2:
+        print("You can travel {} or {}.".format(print_north(),print_south()))
+        direction = ui()
+        if direction != "n" and direction != "s":
+            print("Not a valid directon!")
+        elif direction == "s":
+            position_y = south(position_y)
+        elif direction == "n":
+            position_y = north(position_y)
+
     if position_x == 3 and position_y == 1:
         victory()
         running = False
